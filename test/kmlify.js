9a0682970582ba58a49c814b8eaae947400fe72d
ygjack <ygjack414@hotmail.com>
2019-05-09 08:41:55 +0800

    update test

--- test/kmlify.js
+++ test/kmlify.js
@@ -2,8 +2,7 @@ var fs = require('fs-extra')
 var path = require('path')
 var kmlify = require('../src/index').kmlify
 
-var fileContent = fs.readJsonSync('parse-result.json')
-var json = JSON.parse(fileContent)
-var kmlString = kmlify(json.geoJSON, json.folders)
+var json = fs.readJsonSync('./arcgis-jsons/styled-polyline.json')
+var kmlString = kmlify([json], [], { dataType: 'arcjson' })
 
 fs.outputFileSync(path.resolve(__dirname, `./${Date.now()}.kml`), kmlString)
