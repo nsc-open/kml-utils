e2219491ba011468fcddd3d5636effa36660b5bc
xiaoma12634 <xiaoma12634@yeah.net>
2024-05-31 17:32:33 +0800

    修复 esrijson 转 geojson bug

--- src/convert/geojson/feature.js
+++ src/convert/geojson/feature.js
@@ -5,7 +5,7 @@ var styleProperties = require('./properties').styleProperties
 exports.toFeature = function (graphicJSON) {
   return {
     type: 'Feature',
-    geometry: parse(geoUtils.toWgs84Geometry(graphicJSON.geometry)),
+    geometry: parseEsriGeometry(geoUtils.toWgs84Geometry(graphicJSON.geometry)),
     properties: Object.assign({}, graphicJSON.attributes, styleProperties(graphicJSON))
   }
 }
@@ -16,4 +16,16 @@ exports.toFeatureCollection = function (graphicJSONs) {
     type: 'FeatureCollection',
     features
   }
+}
+
+// INFO: 修复 terraformer-arcgis-parser bug
+function parseEsriGeometry(geometry) {
+  var _geometry = parse(geoUtils.toWgs84Geometry(geometry))
+  // 挖洞：单个 ring 需要转换成 polygon
+  if (geometry.hasOwnProperty('rings') && _geometry.type === 'MultiPolygon') {
+    _geometry.type = 'Polygon'
+    _geometry.coordinates = [_geometry.coordinates[0][0], _geometry.coordinates[1][0]]
+  }
+
+  return _geometry
 }
